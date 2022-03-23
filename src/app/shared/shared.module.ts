788da697e22616c84d50506cc3728c199539b2a8
daniLlobet <neollob@yahoo.es>
2022-03-23 18:39:43 +0100

    chore: added FlexLayoutModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,32 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { MainHeaderComponent } from './components/main-header/main-header.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NameCaseDirective } from './directives/nameCase';
 
 const COMPONENTS = [MainHeaderComponent];
+const THIRD_MODULES = [
+  FlexLayoutModule,
+]
 
 @NgModule({
   declarations: [
-    ...COMPONENTS
+    ...COMPONENTS,
   ],
   imports: [
-    CommonModule, FormsModule, RouterModule, ReactiveFormsModule
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    ReactiveFormsModule,
+    ...THIRD_MODULES
   ],
   exports: [
-    CommonModule, FormsModule, RouterModule, ReactiveFormsModule, ...COMPONENTS
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    ReactiveFormsModule,
+    ...COMPONENTS,
+    ...THIRD_MODULES,
   ]
 })
 export class SharedModule { }
